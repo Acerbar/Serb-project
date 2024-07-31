3347c001315c7723a31bf888a88fd0cbc891671d
Acerbar <acergood1@yandex.ru>
2024-07-31 11:14:24 +0300

    111

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
-  base: "/Serb-project"
+  plugins: [svgr(), react()]
 })
